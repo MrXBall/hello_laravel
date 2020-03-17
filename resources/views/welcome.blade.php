9cb9d55222d13d4f93255b071a1e014909498126
Xiao Qiu <1033637928@qq.com>
2020-03-17 08:24:41 +0000

    Xiao Qiu1 => Xiao Qiu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Xiao Qiu1</div>
+                <div class="title">Hello Laravel! - by Xiao Qiu</div>
             </div>
         </div>
     </body>
